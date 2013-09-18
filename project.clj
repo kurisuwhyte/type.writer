eccc905981a3e630b900fd31ea0989af608ef7d1
Christina Whyte <kurisu.whyte@gmail.com>
2013-09-17 22:59:43 -0300

    Bumps to 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject typewriter "0.2.0"
+(defproject typewriter "0.2.1"
   :description "Old-school documentation, meet Simple."
   :url "http://kurisuwhyte.github.io/type.writer"
   :license {:name "MIT"
